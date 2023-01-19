e4b39fbf5e1278a949e91d04c6c2fc4827deb3a0
epiiaddone <lowlandgiant@yahoo.com>
2023-01-19 20:48:42 +0800

    removed not used logos, fixed the celcius/celsius typo bug

--- temperature-calculator/src/App.js
+++ temperature-calculator/src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import './App.css';
+
+function BoilingVerdict(props){
+  if(props.celsius >= 100) return <p>The water is boiling</p>;
+  else return <p>The water is not boiling</p>;
+}
+
+function celToFar(celsius){
+  return celsius * 1.8 + 32;
+}
+
+function farToCel(farenheit){
+  return (farenheit - 32) / 1.8;
+}
+
+class Calculator extends React.Component{
+  constructor(props){
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.state = {temperature: ''};
+  }
+
+  handleChange(e){
+    this.setState({temperature: e.target.value})
+  }
+
+  render(){
+    const temperature = this.state.temperature;
+    return(
+      <fieldset>
+        <ledgend>Enter Temperature in Celsius</ledgend>
+        <input value={temperature} onChange={this.handleChange} />
+        <BoilingVerdict celsius={parseFloat(temperature)}/>
+      </fieldset>
+    )
+  }
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<Calculator/>
   );
 }
 
